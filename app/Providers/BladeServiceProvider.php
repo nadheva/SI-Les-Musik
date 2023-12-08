fca4b5ea3d9053f66e8dae635af122a76a69139a
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2023-12-08 14:44:37 +0700

    push

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Blade::directive('money', function ($money) {
+            return "<?php echo number_format($money, 2); ?>";
+        });
+    }
+}
---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,15 @@
+
+  <!-- Vendor JS Files -->
+<script src="{{asset('template/NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('template/NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('template/NiceAdmin/assets/vendor/chart.js/chart.umd.js')}}"></script>
+<script src="{{asset('template/NiceAdmin/assets/vendor/echarts/echarts.min.js')}}"></script>
+<script src="{{asset('template/NiceAdmin/assets/vendor/quill/quill.min.js')}}"></script>
+<script src="{{asset('template/NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
+<script src="{{asset('template/NiceAdmin/assets/vendor/tinymce/tinymce.min.js')}}"></script>
+<script src="{{asset('template/NiceAdmin/assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+<script src="{{asset('template/NiceAdmin/assets/js/main.js')}}"></script>
+
+@stack('scripts')
