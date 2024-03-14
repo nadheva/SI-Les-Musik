469d24a6e520bc8e343feb06adef2642415f49e8
nadheva17 <nadheva@ilcs.co.id>
2024-03-14 16:15:57 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -60,9 +60,6 @@ Route::middleware('auth')->group(function () {
     Route::resource('studio', StudioController::class)->except('update');
     Route::put('studio-update/{id}', [StudioController::class, 'update']);
 
-
-    //
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
