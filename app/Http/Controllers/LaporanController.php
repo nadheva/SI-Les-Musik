42e4160ea06a5c3945770c91e2e9da795162b6f2
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2024-07-28 20:29:13 +0700

    push

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -5,9 +5,12 @@ use App\Models\Guru;
 use App\Models\User;
 use App\Models\Role;
 use App\Models\AlatMusik;
+use App\Models\Reservasi;
+use App\Models\Payment;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -18,8 +21,76 @@ class LaporanController extends Controller
      */
     public function index()
     {
-        $laporan = DB::table('reservasi');
-        return view('admin.laporan.index');
+
+       if (Auth::user()->role_id == '1') {
+        $siswa_aktif= DB::table('payment')
+            ->join('reservasi', 'reservasi.id', '=', 'payment.reservasi_id')
+            ->join('course', 'course.id', '=', 'reservasi.course_id')
+            ->join('periode', 'periode.id', '=', 'course.periode_id')
+            ->join('level', 'level.id', '=', 'course.level_id')
+            ->join('users', 'users.id', '=', 'payment.user_id')
+            ->join('profile', 'profile.user_id', '=', 'users.id')
+            ->where('payment.status', '=', 'success')
+            ->where('periode.tgl_akhir_ujian', '>=', Carbon::now())
+            ->select('profile.nama_depan as nama_depan', 'profile.nama_belakang as nama_belakang',
+            'course.judul as judul_course', 'level.nama as level', 'course.harga as harga', 'periode.nama_periode as periode',
+            'periode.tgl_awal_pembelajaran as tgl_join')
+            ->get();
+
+        $siswa_tdk_aktif= DB::table('payment')
+            ->join('reservasi', 'reservasi.id', '=', 'payment.reservasi_id')
+            ->join('course', 'course.id', '=', 'reservasi.course_id')
+            ->join('periode', 'periode.id', '=', 'course.periode_id')
+            ->join('level', 'level.id', '=', 'course.level_id')
+            ->join('users', 'users.id', '=', 'payment.user_id')
+            ->join('profile', 'profile.user_id', '=', 'users.id')
+            ->where('payment.status', '=', 'success')
+            ->where('periode.tgl_akhir_ujian', '<', Carbon::now())
+            ->select('profile.nama_depan as nama_depan', 'profile.nama_belakang as nama_belakang',
+            'course.judul as judul_course', 'level.nama as level', 'course.harga as harga', 'periode.nama_periode as periode',
+            'periode.tgl_awal_pembelajaran as tgl_join')
+            ->get();
+        return view('admin.laporan.index', compact('siswa_aktif', 'siswa_tdk_aktif'));
+        }
+        else if(Auth::user()->role_id == '2') {
+            $user = Auth::user();
+            if(isset($user->profile)){
+                $course_aktif= DB::table('payment')
+                ->join('reservasi', 'reservasi.id', '=', 'payment.reservasi_id')
+                ->join('course', 'course.id', '=', 'reservasi.course_id')
+                ->join('periode', 'periode.id', '=', 'course.periode_id')
+                ->join('level', 'level.id', '=', 'course.level_id')
+                ->join('users', 'users.id', '=', 'payment.user_id')
+                ->join('profile', 'profile.user_id', '=', 'users.id')
+                ->where('payment.status', '=', 'success')
+                ->where('periode.tgl_akhir_ujian', '>=', Carbon::now())
+                ->where('users.id', '=', Auth::user()->id)
+                ->select('profile.nama_depan as nama_depan', 'profile.nama_belakang as nama_belakang',
+                'course.judul as judul_course', 'level.nama as level', 'course.harga as harga', 'periode.nama_periode as periode',
+                'periode.tgl_awal_pembelajaran as tgl_join')
+                ->get();
+
+            $course_tdk_aktif= DB::table('payment')
+                ->join('reservasi', 'reservasi.id', '=', 'payment.reservasi_id')
+                ->join('course', 'course.id', '=', 'reservasi.course_id')
+                ->join('periode', 'periode.id', '=', 'course.periode_id')
+                ->join('level', 'level.id', '=', 'course.level_id')
+                ->join('users', 'users.id', '=', 'payment.user_id')
+                ->join('profile', 'profile.user_id', '=', 'users.id')
+                ->where('payment.status', '=', 'success')
+                ->where('periode.tgl_akhir_ujian', '<', Carbon::now())
+                ->where('users.id', '=', Auth::user()->id)
+                ->select('profile.nama_depan as nama_depan', 'profile.nama_belakang as nama_belakang',
+                'course.judul as judul_course', 'level.nama as level', 'course.harga as harga', 'periode.nama_periode as periode',
+                'periode.tgl_awal_pembelajaran as tgl_join')
+                ->get();
+            return view('user.laporan.index', compact('course_aktif', 'course_tdk_aktif'));
+            }
+            else {
+                Alert::info('Info', 'Silahkan isi profil anda terlebih dahulu!');
+                return redirect()->route('profile.create');
+            }
+        }
     }
 
     /**
--- resources/views/admin/laporan/index.blade.php
+++ resources/views/admin/laporan/index.blade.php
@@ -1,3 +1,172 @@
 <x-app-layout>
-    
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+          <h1>Data Laporan</h1>
+          <nav>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="breadcrumb-item">Tables</li>
+              <li class="breadcrumb-item active">Data</li>
+            </ol>
+          </nav>
+        </div><!-- End Page Title -->
+
+        <section class="section">
+            <div class="row  mt-4">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-header d-flex pb-0 p-3">
+                            <h6 class="my-auto">Data Laporan</h6>
+                            <div class="nav-wrapper position-relative ms-auto w-50">
+                                <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link mb-0 px-0 py-1 active" data-bs-toggle="tab" href="#cam1"
+                                            role="tab" aria-controls="cam1" aria-selected="true">
+                                            Siswa Aktif
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="#cam2" role="tab"
+                                            aria-controls="cam2" aria-selected="false">
+                                            Siswa Tidak Aktif
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card-body p-3 mt-2">
+                            <div class="tab-content" id="v-pills-tabContent">
+                                <div class="tab-pane fade show position-relative active height-400 border-radius-lg"
+                                    id="cam1" role="tabpanel" aria-labelledby="cam1">
+                                    <div class="row mt-4">
+                                        <div class="table-responsive">
+                                            <table class="table datatable align-items-center mb-0" id="datatable-search">
+                                                <thead class="thead-light">
+                                                    <tr>
+                                                        <th><b>N</b>o.</th>
+                                                        <th><b>N</b>ama <b>S</b>iswa</th>
+                                                        <th><b>C</b>ourse</th>
+                                                        <th><b>L</b>evel</th>
+                                                        <th><b>H</b>arga <b>C</b>ourse</th>
+                                                        <th><b>P</b>eriode</th>
+                                                        <th><b>T</b>anggal <b>J</b>oin</th>
+                                                        <th><b>A</b>ksi</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($siswa_aktif as $i)
+                                                        <tr>
+                                                            <td class="text-sm font-weight-normal">{{ $loop->iteration }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->nama_depan." ".$i->nama_belakang}}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->judul_course }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->level }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ Number::currency($i->harga, 'Rp.') }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->periode }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{$i->tgl_join->format('d-m-Y')}}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">
+                                                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#approveReservasi-">Detail
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade show position-relative height-400 border-radius-lg"
+                                    id="cam2" role="tabpanel" aria-labelledby="cam2">
+                                    <div class="row mt-4">
+                                        <div class="table-responsive">
+                                            <table class="table datatable align-items-center mb-0" id="datatable-search1">
+                                                <thead class="thead-light">
+                                                    <tr>
+                                                        <th><b>N</b>o.</th>
+                                                        <th><b>N</b>ama <b>S</b>iswa</th>
+                                                        <th><b>C</b>ourse</th>
+                                                        <th><b>L</b>evel</th>
+                                                        <th><b>H</b>arga <b>C</b>ourse</th>
+                                                        <th><b>P</b>eriode</th>
+                                                        <th><b>T</b>anggal <b>J</b>oin</th>
+                                                        <th><b>A</b>ksi</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($siswa_tdk_aktif as $i)
+                                                        <tr>
+                                                            <td class="text-sm font-weight-normal">{{ $loop->iteration }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->nama_depan." ".$i->nama_belakang}}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->judul_course }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->level }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ Number::currency($i->harga, 'Rp.') }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{ $i->periode }}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">{{$i->tgl_join->format('d-m-Y')}}
+                                                            </td>
+                                                            <td class="text-sm font-weight-normal">
+                                                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#approveReservasi-">Detail
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+      </main><!-- End #main -->
+
+      @push('scripts')
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+
+      <script type="text/javascript">
+        const dataTableSearch = new simpleDatatables.DataTable("#datatable-search", {
+          searchable: true,
+          fixedHeight: true
+        });
+        const dataTableSearch = new simpleDatatables.DataTable("#datatable-search1", {
+          searchable: true,
+          fixedHeight: true
+        });
+
+        $('.show_confirm').click(function(event) {
+                var form =  $(this).closest("form");
+                var name = $(this).data("name");
+                event.preventDefault();
+                swal({
+                    title: `Hapus Data?`,
+                    text: "Jika data terhapus, data akan hilang selamanya!",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                })
+                .then((willDelete) => {
+                  if (willDelete) {
+                    form.submit();
+                  }
+                });
+            });
+
+      </script>
+      @endpush
 </x-app-layout>
