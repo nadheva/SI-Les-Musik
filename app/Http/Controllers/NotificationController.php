8d4a9b73557c1e1737f809fb4331d47586b7d9ce
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2024-08-04 18:01:32 +0700

    notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,64 +1,129 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Payment;
+use Midtrans\Snap;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __construct(Request $request)
     {
-        //
+        $this->middleware('auth')->except('notificationHandler');
+
+        $this->request = $request;
+        // Set midtrans configuration
+        \Midtrans\Config::$serverKey    = config('services.midtrans.serverKey');
+        \Midtrans\Config::$isProduction = config('services.midtrans.isProduction');
+        \Midtrans\Config::$isSanitized  = config('services.midtrans.isSanitized');
+        \Midtrans\Config::$is3ds        = config('services.midtrans.is3ds');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function notificationHandler(Request $request)
     {
-        //
-    }
+        $payload      = $request->getContent();
+        $notification = json_decode($payload);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $validSignatureKey = hash("sha512", $notification->order_id . $notification->status_code . $notification->gross_amount . config('services.midtrans.serverKey'));
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+        if ($notification->signature_key != $validSignatureKey) {
+            return response(['message' => 'Invalid signature'], 403);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+        $transaction  = $notification->transaction_status;
+        $type         = $notification->payment_type;
+        $orderId      = $notification->order_id;
+        $fraud        = $notification->fraud_status;
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        //data tranaction
+        $data_transaction = Payment::where('invoice', $orderId)->first();
+        // $data_transaction1 = SewaStudio::where('invoice', $orderId)->first();
+        // $data_transaction2 = Denda::where('invoice', $orderId)->first();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        if ($transaction == 'capture') {
+
+            // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+            if ($type == 'credit_card') {
+
+              if($fraud == 'challenge') {
+
+                /**
+                *   update invoice to pending
+                */
+                $data_transaction->update([
+                    'status' => 'pending'
+                ]);
+
+
+              } else {
+
+                /**
+                *   update invoice to success
+                */
+                $data_transaction->update([
+                    'status' => 'success'
+                ]);
+
+
+              }
+
+            }
+
+        } elseif ($transaction == 'settlement') {
+
+            /**
+            *   update invoice to success
+            */
+            $data_transaction->update([
+                'status' => 'success'
+            ]);
+
+
+
+        } elseif($transaction == 'pending'){
+
+
+            /**
+            *   update invoice to pending
+            */
+            $data_transaction->update([
+                'status' => 'pending'
+            ]);
+
+
+
+        } elseif ($transaction == ('failure')) {
+
+
+            /**
+            *   update invoice to failed
+            */
+            $data_transaction->update([
+                'status' => 'failed'
+            ]);
+
+
+        } elseif ($transaction == 'expire') {
+
+
+            /**
+            *   update invoice to expired
+            */
+            $data_transaction->update([
+                'status' => 'expired'
+            ]);
+
+
+        } elseif ($transaction == 'cancel') {
+
+            /**
+            *   update invoice to failed
+            */
+            $data_transaction->update([
+                'status' => 'failed'
+            ]);
+
+        }
+
     }
 }
